dd677b4b5e79294957dfc6054d39f44fb2142831
lanteriate <lanteriate@gmail.com>
2015-02-09 00:47:34 +0900

    Completed Ch. 7

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +13,14 @@ public class CrimeActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+		}
 	}
 
 	@Override
